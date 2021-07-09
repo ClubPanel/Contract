8127bd6bb36907be64dd9121173d836c7f556ff5
uellenberg <JonahUellenberg@hotmail.com>
2021-07-09 14:42:36 -0700

    Extend Config

--- config/ContractConfig.ts
+++ config/ContractConfig.ts
@@ -1,4 +1,6 @@
-export interface ContractConfig {
+import {Config} from "../../../shared/config/types/config";
+
+export interface ContractConfig extends Config {
   signURL: string;
   signConfirmURL: string;
   pageName: string;
